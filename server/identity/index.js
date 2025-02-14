ec239972914575476c806da96aaa3b002ea5483e
Jérémy Tarlié <contact@tarlie.fr>
2025-02-14 13:58:16 +0100

    cicd

--- server/identity/index.js
+++ server/identity/index.js
@@ -1,54 +1,66 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
+require("dotenv/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const session = require('express-session');
-const RedisStore = require('connect-redis').default
-const redis = require('redis');
+console.log(process.env.PORT);
+
+const session = require("express-session");
+const RedisStore = require("connect-redis").default;
+const redis = require("redis");
 
 app.use(cors());
-require('dotenv/config')
+
 const db = require("./model/Models");
 
-db.sequelize.sync()
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self'; connect-src 'self' http://localhost:3002"
+  );
+  next();
+});
+
+db.sequelize.sync();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 const redisClient = redis.createClient({
-    username: 'default',
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: 'redis-11219.c339.eu-west-3-1.ec2.redns.redis-cloud.com',
-        port: 11219
-    }
+  username: "default",
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: "redis-11219.c339.eu-west-3-1.ec2.redns.redis-cloud.com",
+    port: 11219,
+  },
 });
 
-redisClient.connect() 
+redisClient.connect();
 
-redisClient.on('error', (err) => {
-    console.error('Erreur Redis:', err);
+redisClient.on("error", (err) => {
+  console.error("Erreur Redis:", err);
 });
 
-
-app.use(session({
+app.use(
+  session({
     store: new RedisStore({ client: redisClient }),
-    secret: process.env.SECRET_KEY, 
-    resave: false, 
-    saveUninitialized: false, 
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false,
     cookie: {
-        httpOnly: true, 
-        maxAge: 24 * 60 * 60 * 1000
-    }
-}));
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    },
+  })
+);
 
 app.get("/", (req, res) => {
-    res.json({ message: "Auth Server" });
+  res.json({ message: "Auth Server" });
 });
 
-require('./routes/auth')(app)
+require("./routes/auth")(app);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
